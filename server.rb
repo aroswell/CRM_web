b1159c4f7f29b873fc43ee9301dac0095ed23ac5
aroswell <aroswell@gmail.com>
2015-04-07 19:58:25 -0400

    Embedded html

--- server.rb
+++ server.rb
@@ -15,6 +15,18 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
+  # client.puts(Time.now.ctime)                       # Output the current time to the client
+  response = "
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>My first web server</title>
+    </head>
+    <body>
+      <h1>Great message from web server</h1>
+      <p>Oh hey, this is my first HTML response!</p>
+    </body>
+</html>"
+  client.puts(response)
   client.close                                      # Disconnect from the client
 end
